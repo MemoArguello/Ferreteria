abc5b64a93619a303706003e3c3517c1f4c1ae80
Guillermo <elmemozac6@gmail.com>
2024-01-31 23:12:02 -0300

    cambios 31012024

--- Frontend/reportes/reporte_cat.php
+++ Frontend/reportes/reporte_cat.php
@@ -23,6 +23,8 @@ mysqli_close($conexiondb);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Categoria</title>
+    <link rel="stylesheet" href="../CSS/dash.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
@@ -40,9 +42,84 @@ mysqli_close($conexiondb);
     </head>
 
 <body>
-    <?php
-    include($_SERVER['DOCUMENT_ROOT'] . '/Sistema_Ferreteria/Frontend/dashboard/inicio.php');
-    ?>
+    	<!-- SIDEBAR -->
+        <section id="sidebar">
+		<a href="../inicio/inicio.php" class="brand">
+			<i class='bx bxs-store-alt' style='color:#3c91e6'></i>
+			<span class="text">Ferreteria</span>
+		</a>
+		<ul class="side-menu top">
+			<li class="">
+				<a href="../inicio/inicio.php">
+					<i class='bx bx-home' style='color:#3c91e6'></i>
+					<span class="text">Inicio</span>
+				</a>
+			</li>
+			<li>
+				<a href="../venta/venta.php">
+					<i class='bx bxs-shopping-bag-alt' style='color:#3c91e6'></i>
+					<span class="text">Ventas</span>
+				</a>
+			</li>
+			<li class="active">
+				<a href="../reportes/reporte_prod.php">
+					<i class='bx bx-cart' style='color:#3c91e6'></i>
+					<span class="text">Productos</span>
+				</a>
+			</li>
+			<li>
+				<a href="../reportes/reporte_prov.php">
+					<i class='bx bxs-package' style='color:#3c91e6'></i>
+					<span class="text">Proveedores</span>
+				</a>
+			</li>
+			<li class="">
+				<a href="../reportes/reporte_caja.php">
+						<i class='bx bx-dollar-circle' style='color:#3c91e6'></i>
+						<span class="text">Caja</span>
+				</a>
+			</li>
+			<li>
+				<a href="../reportes/reporte_cliente.php">
+					<i class='bx bx-group' style='color:#3c91e6'></i>
+					<span class="text">Clientes</span>
+				</a>
+			</li>
+		</ul>
+		<ul class="side-menu">
+			<li>
+				<a href="../reportes/reporte_cuenta.php">
+					<i class='bx bxs-cog' style='color:#3c91e6'></i>
+					<span class="text">Configuracion</span>
+				</a>
+			</li>
+			<li>
+				<a href="../../Backend/validacion/cerrar_sesion.php" class="logout">
+					<i class='bx bxs-log-out-circle'></i>
+					<span class="text">Cerrar Sesión</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+	<!-- SIDEBAR -->
+	<!-- CONTENT -->
+	<section id="content">
+		<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu'></i>
+			<form action="#">
+				<div class="form-input">
+					<input type="search" placeholder="Search...">
+					<button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
+				</div>
+			</form>
+			<input type="checkbox" id="switch-mode" hidden>
+			<label for="switch-mode" class="switch-mode"></label>
+			<a href="#" class="profile">
+				<img src="../IMG/shopping-bag_5939887.png">
+			</a>
+		</nav>
+	</section>
     <section id="content">
         <main>
                 <div class="left">
--- Frontend/venta/venta.php
+++ Frontend/venta/venta.php
@@ -41,10 +41,10 @@ if ($resultadoUsuario) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ventas</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../IMG/nut_5361285.png" rel="icon">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
+    <link rel="stylesheet" href="../CSS/dash.css">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 
     <style>
         .form_vent {
@@ -153,9 +153,84 @@ if ($resultadoUsuario) {
 </head>
 
 <body>
-    <?php
-    include($_SERVER['DOCUMENT_ROOT'] . '/Sistema_Ferreteria/Frontend/dashboard/inicio.php');
-    ?>
+    	<!-- SIDEBAR -->
+        <section id="sidebar">
+		<a href="../inicio/inicio.php" class="brand">
+			<i class='bx bxs-store-alt' style='color:#3c91e6'></i>
+			<span class="text">Ferreteria</span>
+		</a>
+		<ul class="side-menu top">
+			<li class="">
+				<a href="../inicio/inicio.php">
+					<i class='bx bx-home' style='color:#3c91e6'></i>
+					<span class="text">Inicio</span>
+				</a>
+			</li>
+			<li class="active">
+				<a href="../venta/venta.php">
+					<i class='bx bxs-shopping-bag-alt' style='color:#3c91e6'></i>
+					<span class="text">Ventas</span>
+				</a>
+			</li>
+			<li class="">
+				<a href="../reportes/reporte_prod.php">
+					<i class='bx bx-cart' style='color:#3c91e6'></i>
+					<span class="text">Productos</span>
+				</a>
+			</li>
+			<li>
+				<a href="../reportes/reporte_prov.php">
+					<i class='bx bxs-package' style='color:#3c91e6'></i>
+					<span class="text">Proveedores</span>
+				</a>
+			</li>
+			<li class="">
+				<a href="../reportes/reporte_caja.php">
+						<i class='bx bx-dollar-circle' style='color:#3c91e6'></i>
+						<span class="text">Caja</span>
+				</a>
+			</li>
+			<li>
+				<a href="../reportes/reporte_cliente.php">
+					<i class='bx bx-group' style='color:#3c91e6'></i>
+					<span class="text">Clientes</span>
+				</a>
+			</li>
+		</ul>
+		<ul class="side-menu">
+			<li>
+				<a href="../reportes/reporte_cuenta.php">
+					<i class='bx bxs-cog' style='color:#3c91e6'></i>
+					<span class="text">Configuracion</span>
+				</a>
+			</li>
+			<li>
+				<a href="../../Backend/validacion/cerrar_sesion.php" class="logout">
+					<i class='bx bxs-log-out-circle'></i>
+					<span class="text">Cerrar Sesión</span>
+				</a>
+			</li>
+		</ul>
+	</section>
+	<!-- SIDEBAR -->
+	<!-- CONTENT -->
+	<section id="content">
+		<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu'></i>
+			<form action="#">
+				<div class="form-input">
+					<input type="search" placeholder="Search...">
+					<button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
+				</div>
+			</form>
+			<input type="checkbox" id="switch-mode" hidden>
+			<label for="switch-mode" class="switch-mode"></label>
+			<a href="#" class="profile">
+				<img src="../IMG/shopping-bag_5939887.png">
+			</a>
+		</nav>
+	</section>
     <section id="content">
         <main>
                 <div class="left">
